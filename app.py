7262153e0e1ef6e671cd1bff163b7adcdf90d603
mtahirmunir <39256218+mtahirmunir@users.noreply.github.com>
2024-11-20 23:15:54 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+import streamlit as st
+from langchain_community.llms import Ollama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+## Langsmith Tracking
+## os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
+## os.environ["LANGCHAIN_TRACING_V2"] = "true"
+## os.environ["LANGCHAIN_PROJECT"] = st.secrets["LANGCHAIN_PROJECT"]
+
+## Prompt Template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please respond to the question asked"),
+        ("user", "Question:{question}")
+    ]
+)
+
+## Streamlit framework
+st.title("Langchain Demo With Gemma Model")
+input_text = st.text_input("What question you have in mind?")
+
+## Ollama Llama2 model
+llm = Ollama(model="gemma:2b")
+output_parser = StrOutputParser()
+chain = prompt | llm | output_parser
+
+if input_text:
+    st.write(chain.invoke({"question": input_text}))
